5ddd3d9ab625682c7b12454d4dd39c9cd89f3e54
Daedalus <manny.is.daedalus@pm.me>
2024-01-09 15:46:45 +0000

    fixed /status endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,10 @@
 """the Flask app's index"""
 
 from api.v1.views import app_views
-import json
+from flask import jsonify
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """returns the status on app_views"""
-    status = { "status": 'OK' }
-    return json.dumps(status)
+    status = { "status": "OK" }
+    return jsonify(status)
